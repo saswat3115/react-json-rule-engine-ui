26343ff4cb803b45b476a2a72ffab5a7b001bb29
phenom_saswat_mohanty <saswat.mohanty@phenompeople.com>
2020-10-16 13:22:04 +0530

    commit dist for github page

---
+++ build/precache-manifest.db7c692eb5c1961aa32a4e87f435bb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdafe2d468f589001759ed95273543c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf13bbbceb8fc9bedb2",
+    "url": "/static/css/main.6f6bbf59.chunk.css"
+  },
+  {
+    "revision": "98bd7459082f40d2d6b0",
+    "url": "/static/js/2.944dd74d.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "/static/js/2.944dd74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf13bbbceb8fc9bedb2",
+    "url": "/static/js/main.d08c4a75.chunk.js"
+  },
+  {
+    "revision": "782472215917181ba5bf",
+    "url": "/static/js/runtime-main.997a7de9.js"
+  }
+]);
